838f13995b1b2684d67d1a14cf3089eba6dffc43
NomadV3 <nom4dv3@gmail.com>
2023-08-17 16:16:32 +0800

    add prove api set, publish 0.0.6

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 export { execute } from "./api/exec.js";
+export { proveInputGen } from "./api/prove_inputgen.js";
+export { proveMock } from "./api/prove_mock.js";
+export { prove } from "./api/prove.js";
