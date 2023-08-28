e30906ee91e8f0f051c64327118a3e2799dad1a3
Norman <nom4dv3@gmail.com>
2023-08-28 19:50:11 +0800

    release 0.0.19

--- tests/getRawReceipts.js
+++ tests/getRawReceipts.js
@@ -1,8 +1,9 @@
 import {ethers} from "ethers";
 import { config } from "./config.js";
-import { getRawReceipts } from "../common/ethers_helper.js";
+// import * as zkgapi from "../index.js"
+import * as zkgapi from "@hyperoracle/zkgraph-api"
 
 const provider = new ethers.providers.JsonRpcProvider(config.Provider.mainnet);
 
-let res = await getRawReceipts(provider, 17967861, false)
+let res = await zkgapi.getRawReceipts(provider, 17967861, false)
 console.log(res);
