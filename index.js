9ab231acc5a48071c7903f358c9933ad9cc502b8
Suning Yao <sy2940@nyu.edu>
2023-09-03 19:55:05 +0800

    export waitTaskStatus

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-export { execute, executeOnRawReceipts } from "./api/exec.js";
-export { proveInputGen, proveInputGenOnRawReceipts } from "./api/prove_inputgen.js";
-export { proveMock } from "./api/prove_mock.js";
-export { prove } from "./api/prove.js";
-export { upload } from "./api/upload.js";
-export { setup } from "./api/setup.js";
-export { compile, compileInner } from "./api/compile.js";
-export { deploy } from "./api/deploy.js";
-export { publish } from "./api/publish.js";
-export { verify } from "./api/verify.js";
-export { getRawReceipts, getBlockByNumber } from "./common/ethers_helper.js";
+export { execute, executeOnRawReceipts } from './api/exec.js';
+export {
+  proveInputGen,
+  proveInputGenOnRawReceipts,
+} from './api/prove_inputgen.js';
+export { proveMock } from './api/prove_mock.js';
+export { prove } from './api/prove.js';
+export { upload } from './api/upload.js';
+export { setup } from './api/setup.js';
+export { compile, compileInner } from './api/compile.js';
+export { deploy } from './api/deploy.js';
+export { publish } from './api/publish.js';
+export { verify } from './api/verify.js';
+export { getRawReceipts, getBlockByNumber } from './common/ethers_helper.js';
+export { waitTaskStatus } from './requests/zkwasm_taskdetails.js';
