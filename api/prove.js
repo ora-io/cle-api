92602cda01caa6ac5a053ae26c5c74fee76c6f50
Suning Yao <sy2940@nyu.edu>
2023-09-08 21:14:27 +0800

    fix(prove): remove error in returned value

--- api/prove.js
+++ api/prove.js
@@ -18,10 +18,9 @@ import {
  * @param {string} zkwasmProverUrl - the url of the zkwasm prover
  * @param {string} userPrivateKey - the acct for sign&submi prove task to zkwasm
  * @param {boolean} enableLog - enable logging or not
- * @returns {[string, object]} - 1st arg: err, return null when succ, string when failed; 2nd arg: proof details in json
+ * @returns {object} - proof details in json
  */
 export async function prove(wasmPath, privateInputStr, publicInputStr, zkwasmProverUrl, userPrivateKey, enableLog=true) {
-    let err = null;
     let result = {"instances":null, "batch_instances":null, "proof":null, "aux":null, "md5": null, "taskId": null};
 
     // Prove mode
@@ -76,87 +75,44 @@ export async function prove(wasmPath, privateInputStr, publicInputStr, zkwasmPro
       } catch (error) {
         loading.stopAndClear();
         throw error;
-        // console.error(error);
-        // process.exit(1);
       }
 
       if (taskDetails.status === "Done") {
 
         if (enableLog) {
             loading.stopAndClear();
-
             console.log("[+] PROVE SUCCESS!", "\n");
         }
-
 
         const instances = toHexStringBytes32Reverse(taskDetails.instances);
         const batch_instances = toHexStringBytes32Reverse(taskDetails.batch_instances);
         const proof = toHexStringBytes32Reverse(taskDetails.proof);
         const aux = toHexStringBytes32Reverse(taskDetails.aux);
-
-        // write proof to file as txt
-        // let outputProofFile = `build/proof_${taskId}.txt`;
-
-        // if (enableLog) {
-        //     console.log(`[+] Proof written to ${outputProofFile} .\n`);
-        // }
-
-        // writeFileSync(
-        //   outputProofFile,
-        //   "Instances:\n" +
-        //     instances +
-        //     "\n\nProof transcripts:\n" +
-        //     proof +
-        //     "\n\nAux data:\n" +
-        //     aux +
-        //     "\n",
-        // );
-
         if (enableLog) {
             taskPrettyPrint(taskDetails, "[*] ");
 
-            // Log extra new line before divider.
             console.log();
         }
-
-
-        // logDivider();
-
-        err = null
         result['instances'] = instances
         result['batch_instances'] = batch_instances
         result['proof'] = proof
         result['aux'] = aux
         result['taskId'] = taskId
-
-        // process.exit(0);
       } else {
 
-        err = taskDetails.internal_message
         result['taskId'] = taskId
 
         if (enableLog) {
             loading.stopAndClear();
 
             console.log("[-] PROVE OR DRYRUN FAILED.", "\n");
-
-            console.log(`[-] ${err}`, "\n");
         }
-        // logDivider();
-
-        // process.exit(1);
       }
     } else {
-
-        err = errorMessage
-
         if (enableLog) {
           console.log(`[-] PROVE CANNOT BE STARTED. MIGHT NEED TO SETUP`, "\n");
-          // Log status
-          console.log(`[-] ${err}`, "\n");
         }
-    //   logDivider();
     }
 
-    return [err, result]
+    return result;
 }
