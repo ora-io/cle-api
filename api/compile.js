5a306f060bcd6f301c4fcd69fce83736e6a2a9eb
Norman <nom4dv3@gmail.com>
2023-10-31 17:49:19 +0800

    refactor compile entry to main_event_full/local

--- api/compile.js
+++ api/compile.js
@@ -11,7 +11,7 @@ const wasmStartName = "__as_start"
 
 export function compileInner(){
     const commands = [
-      `npx asc node_modules/@hyperoracle/zkgraph-lib/common/inner.ts -o ${innerPrePrePath} --runtime stub --use abort=node_modules/@hyperoracle/zkgraph-lib/common/type/abort --disable bulk-memory --disable mutable-globals --exportRuntime --exportStart ${wasmStartName}  --memoryBase 70000`,
+      `npx asc node_modules/@hyperoracle/zkgraph-lib/main_event_full.ts -o ${innerPrePrePath} --runtime stub --use abort=node_modules/@hyperoracle/zkgraph-lib/common/type/abort --disable bulk-memory --disable mutable-globals --exportRuntime --exportStart ${wasmStartName}  --memoryBase 70000`,
     ];
 
     const combinedCommand = commands.join(" && ");
@@ -42,7 +42,7 @@ export async function compile(wasmPath, watPath, mappingPath, yamlPath, compiler
   // Local Compile
   if (isLocal === true) {
     const commands = [
-      `npx asc node_modules/@hyperoracle/zkgraph-lib/main_local.ts -t ${watPath} -O --noAssert -o ${wasmPath} --disable bulk-memory --use abort=node_modules/@hyperoracle/zkgraph-lib/common/type/abort --exportRuntime --runtime stub --memoryBase 70000`, // note: need --exportRuntime or --bindings esm; (--target release)
+      `npx asc node_modules/@hyperoracle/zkgraph-lib/main_event_local.ts -t ${watPath} -O --noAssert -o ${wasmPath} --disable bulk-memory --use abort=node_modules/@hyperoracle/zkgraph-lib/common/type/abort --exportRuntime --runtime stub --memoryBase 70000`, // note: need --exportRuntime or --bindings esm; (--target release)
     ];
     const combinedCommand = commands.join(" && ");
 
