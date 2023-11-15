cc42bbab9d112ed85aba28c64ec8fad0c3a7990d
Norman <nom4dv3@gmail.com>
2023-11-15 13:40:04 +0800

    refactor(test): clean healthcheck test

--- tests/testHealthCheck.js
+++ tests/testHealthCheck.js
@@ -1,43 +1,16 @@
 import { ZkGraphYaml } from "../type/zkgyaml.js";
 
-try{
-    const yaml1 = ZkGraphYaml.fromYamlPath('tests/testsrc/zkgraph.yaml');
-    yaml1.yamlhealthCheck();
-    console.log("valid yaml1");
+function test_healthcheck(yamlPath) {
+  try{
+      const yaml1 = ZkGraphYaml.fromYamlPath(yamlPath);
+      yaml1.healthCheck();
+      console.log("valid:", yamlPath);
 
-} catch(e) {
-    console.log(e);
+  } catch(e) {
+      console.log(e);
+  }
 }
 
-try{
-    const yaml2 = ZkGraphYaml.fromYamlPath('tests/testsrc/zkgraph2.yaml');
-    yaml2.yamlhealthCheck();
-    console.log("valid yaml2");
-
-} catch(e) {
-    console.log(e);
-}
-
-try {
-    const yaml3 = ZkGraphYaml.fromYamlPath('tests/testsrc/zkgraph3.yaml');
-    yaml3.yamlhealthCheck();
-    console.log("valid yaml3");
-} catch(e) {
-    console.log(e);
-}
-
-// try {
-//     const yaml4 = loadYaml('tests/testsrc/zkgraph4.yaml');
-//     yamlhealthCheck(yaml4);
-//     console.log("valid yaml4");
-// } catch(e) {
-//     console.log(e);
-// }
-
-// try {
-//     const yaml5 = loadYaml('tests/testsrc/zkgraph5.yaml');
-//     yamlhealthCheck(yaml5);
-//     console.log("valid yaml5");
-// } catch(e) {
-//     console.log(e);
-// }
+test_healthcheck('tests/testsrc/zkgraph-event.yaml')
+test_healthcheck('tests/testsrc/zkgraph-storage.yaml')
+test_healthcheck('tests/testsrc/zkgraph-dirty.yaml')
