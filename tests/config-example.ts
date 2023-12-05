70e067bd002392a26b73ab1cce5113f1c5e9b211
Norman <nom4dv3@gmail.com>
2023-12-05 16:57:01 +0800

    chore(tests): config.provider->JsonRpcProviderUrl

--- tests/config-example.ts
+++ tests/config-example.ts
@@ -1,5 +1,5 @@
 export const config = {
-  provider: { // Erigon node rpc are highly recommended here.
+  JsonRpcProviderUrl: { // Erigon node rpc are highly recommended here.
     mainnet: 'https://eth.llamarpc.com',
     sepolia: 'https://eth-sepolia.public.blastapi.io',
   },
