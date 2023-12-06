2761c5ab5593550fa6628e5898a07f5cdc133c0b
MuRong <372638156@qq.com>
2023-12-06 10:22:45 +0800

    test(deposit): use provider pass to deposit api

--- tests/deposit.test.ts
+++ tests/deposit.test.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import { it } from 'vitest'
+import { ethers } from 'ethers'
 import * as zkgapi from '../src/index'
 import { config } from './config'
 
@@ -8,10 +9,11 @@ import { config } from './config'
 it('test deposit', async () => {
   const enableLog = true
   const rpcUrl = config.JsonRpcProviderUrl.sepolia
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
   const deployedContractAddress = '0x870ef9B5DcBB6F71139a5f35D10b78b145853e69'
   const depositAmount = '0.001'
   const userPrivateKey = config.UserPrivateKey
-  const result = await zkgapi.deposit(rpcUrl, deployedContractAddress, depositAmount, userPrivateKey, enableLog)
+  const result = await zkgapi.deposit(provider, deployedContractAddress, depositAmount, userPrivateKey, enableLog)
 
   console.log(result)
 }, { timeout: 100000 })
