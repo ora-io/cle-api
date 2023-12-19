d0e300cc902e4d15f6c8292c1a3d36d46fca9ed4
Norman <nom4dv3@gmail.com>
2023-12-19 18:50:16 +0800

    chore(yaml): fix event/slots types

--- src/types/zkgyaml_eth.ts
+++ src/types/zkgyaml_eth.ts
@@ -12,8 +12,23 @@ function isEthereumAddress(address: string) {
     return false
   }
 }
-
-class EventSection {
+class EventItem {
+  constructor(
+    public address: string,
+    public events: string[],
+  ) {
+    // this.address = address.toLocaleLowerCase()
+  }
+}
+class StorageItem {
+  constructor(
+    public address: string,
+    public slots: string[], // ethers.utils.BytesLike[]
+  ) {
+    // this.address = address.toLocaleLowerCase()
+  }
+}
+class EventSectionCache {
   constructor(
     public addressList: string[],
     public esigsList: string[][],
@@ -21,8 +36,7 @@ class EventSection {
     this.addressList = addressList.map(item => item.toLocaleLowerCase())
   }
 }
-
-class StorageSection {
+class StorageSectionCache {
   constructor(
     public addressList: string[],
     public slotsList: string[][],
@@ -35,13 +49,13 @@ export class EthereumDataSource extends DataSource {
   network: string
   // event: EventSection | null
   // storage: StorageSection | null
-  event: any[] | null
-  storage: any[] | null
+  event: EventItem[] | null
+  storage: StorageItem[] | null
   account: any[] | null
   block: any
 
-  eventSectionCache: EventSection | null = null
-  storageSectionCache: StorageSection | null = null
+  eventSectionCache: EventSectionCache | null = null
+  storageSectionCache: StorageSectionCache | null = null
 
   constructor(
     kind: DataSourceKind,
@@ -59,7 +73,7 @@ export class EthereumDataSource extends DataSource {
     this.block = block
   }
 
-  static from_v_0_0_2(yamlEthDS: { kind: DataSourceKind; network: string; event: EventSection[]; storage: StorageSection[] }) {
+  static from_v_0_0_2(yamlEthDS: { kind: DataSourceKind; network: string; event: EventItem[]; storage: StorageItem[] }) {
     return new EthereumDataSource(
       yamlEthDS.kind,
       yamlEthDS.network,
@@ -84,9 +98,9 @@ export class EthereumDataSource extends DataSource {
   getEventLists(useCache = true) {
     // return if there's cache, cause it's always the same
     if (!useCache || this.eventSectionCache == null) {
-      const loadFromEventSource = (event: { address: any; events: any[] }) => {
-        const source_address = event.address
-        const source_esigs = event.events.map((ed: string) => {
+      const loadFromEventSource = (eventItem: EventItem) => {
+        const source_address = eventItem.address
+        const source_esigs = eventItem.events.map((ed: string) => {
           const eventHash = ed.startsWith('0x') ? ed : ethers.utils.keccak256(ethers.utils.toUtf8Bytes(ed))
           return eventHash
         })
@@ -99,7 +113,7 @@ export class EthereumDataSource extends DataSource {
       if (this.event)
         this.event.forEach((event: any) => { const [sa, se] = loadFromEventSource(event); eventDSAddrList.push(sa); eventDSEsigsList.push(se) })
 
-      this.eventSectionCache = new EventSection(eventDSAddrList, eventDSEsigsList)
+      this.eventSectionCache = new EventSectionCache(eventDSAddrList, eventDSEsigsList)
     }
     return [this.eventSectionCache.addressList, this.eventSectionCache.esigsList] as [string[], string[][]]
   }
@@ -107,7 +121,7 @@ export class EthereumDataSource extends DataSource {
   getStorageLists(useCache = true) {
     // return if there's cache, cause it's always the same
     if (!useCache || this.storageSectionCache == null) {
-      const loadFromStorageSource = (storage: { address: string; slots: ethers.utils.BytesLike[] }) => {
+      const loadFromStorageSource = (storage: StorageItem) => {
         const source_address = storage.address.toLocaleLowerCase()
         const source_slots = storage.slots.map((sl: ethers.utils.BytesLike) => {
           return ethers.utils.hexZeroPad(sl, 32)
@@ -125,7 +139,7 @@ export class EthereumDataSource extends DataSource {
           stateDSSlotsList.push(sl)
         })
       }
-      this.storageSectionCache = new StorageSection(stateDSAddrList, stateDSSlotsList)
+      this.storageSectionCache = new StorageSectionCache(stateDSAddrList, stateDSSlotsList)
     }
     return [this.storageSectionCache.addressList, this.storageSectionCache.slotsList] as [string[], string[][]]
   }
