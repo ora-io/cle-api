18d3bb68629953fcd456966b720f1891f6e12792
MuRong <hi@mrong.me>
2024-01-04 11:54:42 +0800

    chore: fix throw genStreamAndMatchedEventOffsets

--- src/common/api_helper.ts
+++ src/common/api_helper.ts
@@ -45,8 +45,8 @@ export function genStreamAndMatchedEventOffsets(rawreceiptList: any[], eventList
   let accumulateReceiptLength = 0
   let rawreceipts = ''
 
-  if (rawreceiptList.length === eventList.length)
-    throw new Error('rawreceiptList and eventList not should have same length.')
+  if (rawreceiptList.length !== eventList.length)
+    throw new Error('rawreceiptList and eventList should have same length.')
 
   for (const rcpid in rawreceiptList) {
     const es = eventList[rcpid]
