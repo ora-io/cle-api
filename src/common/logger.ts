817290defef8bc95fb53cfa3f81283d6a215717b
MuRong <hi@mrong.me>
2024-02-07 17:18:21 +0800

    chore: set default logger

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -5,26 +5,8 @@ export abstract class CLELogger {
   abstract error(...args: any[]): void
 }
 
-export class CLEConsoleLogger extends CLELogger {
-  debug(...args: any[]) {
-    console.debug(...args)
-  }
-
-  info(...args: any[]) {
-    console.info(...args)
-  }
-
-  warn(...args: any[]) {
-    console.warn(...args)
-  }
-
-  error(...args: any[]) {
-    console.error(...args)
-  }
-}
-
 // eslint-disable-next-line import/no-mutable-exports
-export let logger = new CLEConsoleLogger()
+export let logger = console as CLELogger
 
 export function setCLELogger(newLogger: CLELogger) {
   logger = newLogger
