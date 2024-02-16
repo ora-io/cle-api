a737ca460afd5bc3e604bfbf6685da8514b5f7df
Norman <nom4dv3@gmail.com>
2024-02-16 16:57:40 +0800

    test: fix fixtures mapping import

--- tests/fixtures/dsp/ethereum(storage)/mapping.ts
+++ tests/fixtures/dsp/ethereum(storage)/mapping.ts
@@ -1,5 +1,5 @@
-import { Block } from '@hyperoracle/cle-lib-test'
-import { Bytes } from '@hyperoracle/cle-lib-test'
+import { Block } from '@ora-io/cle-lib'
+import { Bytes } from '@ora-io/cle-lib'
 
 export function handleBlocks(blocks: Block[]): Bytes {
   return blocks[0].accounts[0].address
