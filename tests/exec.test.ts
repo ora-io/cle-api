1ba89af3d47fc56c0d547a41736bc2fc08fc276f
Norman <nom4dv3@gmail.com>
2024-05-06 22:14:54 +0800

    fix(test): dsp import trigger unexpected tests

--- tests/exec.test.ts
+++ tests/exec.test.ts
@@ -7,6 +7,7 @@ import { DSPNotFound } from '../src/common/error'
 import { config } from './config'
 import { loadYamlFromPath } from './utils/yaml'
 import { fixtures } from './fixureoptions'
+import { testExecute } from './exec_test_impl'
 
 (global as any).__BROWSER__ = false
 
@@ -17,35 +18,6 @@ const option = fixtures[pathfromfixtures]
 // enable this to silence logs
 // cleapi.setCLELogger(new cleapi.SilentLogger())
 
-export async function testExecute(option: any) {
-  const { wasmPath, yamlPath, expectedState, blocknum } = option
-
-  const wasm = fs.readFileSync(wasmPath)
-  const wasmUint8Array = new Uint8Array(wasm)
-  // const yamlContent = fs.readFileSync(yamlPath, 'utf-8')
-  const yaml = loadYamlFromPath(yamlPath) as cleapi.CLEYaml
-  const dsp = cleapi.dspHub.getDSPByYaml(yaml, { })
-  // const dsp = cleapi.dspHub.getDSPByYaml(yaml, { isLocal: false })
-
-  const jsonRpcUrl = loadConfigByNetwork(yaml, config.JsonRpcProviderUrl, true)
-  const provider = new providers.JsonRpcProvider(jsonRpcUrl)
-  const generalParams = {
-    provider,
-    blockId: loadConfigByNetwork(yaml, blocknum, true), // for storage
-    // blockId: loadConfigByNetwork(yaml, blocknumForEventTest, true), // for event
-  }
-
-  const execParams = dsp?.toExecParams(generalParams)
-
-  const state = await cleapi.execute(
-    { wasmUint8Array, cleYaml: yaml },
-    execParams as any,
-  )
-  // console.log(toHexString(state))
-
-  expect(toHexString(state)).toEqual(expectedState)
-  return state
-}
 describe('test exec', () => {
   it('test exec', async () => {
     await testExecute(option)
